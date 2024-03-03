ed48f22ed7e8d5b85a7dc11d4edb50ed45104242
Ella Robinson <ellaroosmarijn@gmail.com>
2024-03-03 22:56:30 +1300

    add placeholders for other pages with redirects to them from the header

---
+++ src/app/socials/page.tsx
@@ -0,0 +1,3 @@
+export default function SocialsPage() {
+  return <h1>SOCIALS PAGE</h1>
+}
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,13 +4,13 @@ import styles from './header.module.css'
 export function Header() {
   return (
     <header className={styles['header-container']}>
-      <Link href={''} className={styles.link}>
+      <Link href={'/projects'} className={styles.link}>
         Projects
       </Link>
-      <Link href={''} className={styles.link}>
+      <Link href={'/socials'} className={styles.link}>
         Socials
       </Link>
-      <Link href={''} className={styles.link}>
+      <Link href={'/contact'} className={styles.link}>
         Contact
       </Link>
     </header>
