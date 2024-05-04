a1bbab927a321de5b6cf1f966944139994bbc7dc
Ella Robinson <ellaroosmarijn@gmail.com>
2024-05-04 14:33:47 +0200

    feat: add button component with styling

---
+++ src/components/button/button.tsx
@@ -0,0 +1,41 @@
+import Link, { LinkProps } from "next/link"
+import { ComponentProps } from "react"
+import styles from "./button.module.css"
+import cx from "classnames"
+
+export type ButtonProps = {
+  loading?: boolean
+  href?: string
+  label: string
+  theme?: "primary" | "secondary" | "ghost"
+  disabled?: boolean
+} & ComponentProps<"button"> &
+  Partial<LinkProps>
+
+export function Button({
+  loading,
+  disabled,
+  label,
+  theme = "primary",
+  href,
+  className,
+  ...props
+}: ButtonProps) {
+  const El = href ? Link : "button"
+
+  return (
+    <El
+      to={href}
+      className={cx(
+        styles.button,
+        styles[theme],
+        loading && styles.loading,
+        disabled && styles.disabled,
+        className
+      )}
+      {...(props as any)}
+    >
+      {label}
+    </El>
+  )
+}
