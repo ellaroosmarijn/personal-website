d6b7302d0a53467b29447edd8ead94ec30e1f3ee
Ella Robinson <ellaroosmarijn@gmail.com>
2024-05-04 12:48:32 +0200

    feat: add the 'inter' font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-import { Header } from '@/components/header/header'
-// import type { Metadata } from 'next'
-import { Footer } from '@/components/footer/footer'
-import { Inter } from 'next/font/google'
-import '../styles/variables.css'
-import './global.css'
+import { Header } from "@/components/header/header"
+import { Footer } from "@/components/footer/footer"
+import { Inter } from "next/font/google"
+import "../styles/variables.css"
+import "./global.css"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 // export const metadata: Metadata = {
 // title: '',
@@ -19,6 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>
         <main>
           <Header />
