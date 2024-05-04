362501839ef00fd98d8f6c31ef95eb1b58992525
Ella Robinson <ellaroosmarijn@gmail.com>
2024-05-04 21:08:20 +0200

    test: button applys themes styling

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -3,12 +3,36 @@ import { render, screen } from "@testing-library/react"
 import { Button } from "./button"
 
 describe("Button", () => {
-  it("renders a heading", () => {
-    const label = "Test Button"
+  it("renders with a label", () => {
+    const label = "Test button"
     render(<Button {...{ label }} />)
 
     const button = screen.getByRole("button", { name: label })
 
     expect(button).toBeInTheDocument()
+  })
+
+  it("should apply themes", () => {
+    render(
+      <>
+        <Button theme="primary" label="Primary button" />
+        <Button theme="secondary" label="Secondary button" />
+        <Button theme="ghost" label="Ghost button" />
+      </>
+    )
+
+    const secondaryButton = screen.getByText("Secondary button")
+    const primaryButton = screen.getByText("Primary button")
+    const ghostButton = screen.getByText("Ghost button")
+
+    expect(primaryButton).toHaveStyle({
+      backgroundColor: "var(--color-olive-700)",
+    })
+
+    expect(secondaryButton).toHaveStyle({
+      backgroundColor: "var(--color-olive-500)",
+    })
+
+    expect(ghostButton).toHaveClass("ghost")
   })
 })
