9c21d15a3073cf0e75808bc02bf91b58f346a3ab
Ella Robinson <ellaroosmarijn@gmail.com>
2024-05-05 19:50:44 +0200

    refactor: footer to no longer use the cta

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,23 +1,31 @@
-import Link from 'next/link'
-import { CallToAction } from '../call-to-action/call-to-action'
-import styles from './footer.module.css'
+"use client"
+
+import Link from "next/link"
+import styles from "./footer.module.css"
+import cx from "classnames"
+import { useState } from "react"
 
 export function Footer() {
+  const [modalOpen, setModalOpen] = useState(false)
+
   return (
-    <footer className={styles['footer-container']}>
-      <CallToAction
-        className={styles.cta}
-        title={'Contact me'}
-        subtitle={`Let's get in touch!`}
-      />
-      <div className={styles['link-container']}>
-        <Link href={'/projects'} className={styles.link}>
+    <footer className={styles["footer-container"]}>
+      <div className={cx(styles["link-container"], styles.contact)}>
+        <h2
+          className={cx(styles.title, styles.link)}
+          onClick={() => setModalOpen(!modalOpen)}
+        >
+          Let's get in touch!
+        </h2>
+      </div>
+      <div className={styles["link-container"]}>
+        <Link href={"/projects"} className={styles.link}>
           Projects
         </Link>
-        <Link href={'/socials'} className={styles.link}>
+        <Link href={"/socials"} className={styles.link}>
           Socials
         </Link>
-        <Link href={'/contact'} className={styles.link}>
+        <Link href={"/contact"} className={styles.link}>
           Contact
         </Link>
       </div>
