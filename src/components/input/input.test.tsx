02f367074b88d1fd81de69d7530932b5a8f17dde
Ella Robinson <ellaroosmarijn@gmail.com>
2024-05-06 20:50:44 +0200

    refactor: tests to use variables - DRY

--- src/components/input/input.test.tsx
+++ src/components/input/input.test.tsx
@@ -2,34 +2,36 @@ import React from "react"
 import { render, fireEvent, screen } from "@testing-library/react"
 import { Input } from "./input"
 
+const placeholder = "Enter text"
+const inputValue = "Hello world"
+const errorMessage = "Invalid input"
+
 describe("Input component", () => {
   it("renders with different props", () => {
     render(
       <Input
-        placeholder="Enter text"
+        placeholder={placeholder}
         invalid={false}
         disabled={true}
         info="Extra information"
         clearable={true}
-        value={"Hello world"}
+        value={inputValue}
       />
     )
 
     expect(screen.getByPlaceholderText("Enter text")).toBeInTheDocument()
     expect(screen.getByTestId("input-clear")).toBeInTheDocument()
     expect(screen.getByText("Extra information")).toBeInTheDocument()
-    expect(screen.getByText("Hello world")).toBeInTheDocument()
   })
 
   it("clears the input value when the X icon is clicked", () => {
-    const placeholder = "Enter text"
-    const inputValue = "Hello world"
-
     render(
       <Input placeholder={placeholder} value={inputValue} clearable={true} />
     )
 
-    expect(screen.getByPlaceholderText(placeholder)).toHaveValue(inputValue)
+    const input = screen.getByPlaceholderText(placeholder)
+
+    expect(input).toHaveValue(inputValue)
 
     fireEvent.click(screen.getByTestId("input-clear"))
 
