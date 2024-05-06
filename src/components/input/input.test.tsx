196fda84b5765a53a8045b02d583f79dab821ccc
Ella Robinson <ellaroosmarijn@gmail.com>
2024-05-06 20:31:06 +0200

    test: clears the input value when the X icon is clicked

--- src/components/input/input.test.tsx
+++ src/components/input/input.test.tsx
@@ -20,4 +20,19 @@ describe("Input component", () => {
     expect(screen.getByText("Extra information")).toBeInTheDocument()
     expect(screen.getByText("Hello world")).toBeInTheDocument()
   })
+
+  it("clears the input value when the X icon is clicked", () => {
+    const placeholder = "Enter text"
+    const inputValue = "Hello world"
+
+    render(
+      <Input placeholder={placeholder} value={inputValue} clearable={true} />
+    )
+
+    expect(screen.getByPlaceholderText(placeholder)).toHaveValue(inputValue)
+
+    fireEvent.click(screen.getByTestId("input-clear"))
+
+    expect(screen.getByPlaceholderText(placeholder)).toHaveValue("")
+  })
 })
