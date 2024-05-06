63fb7655bd5b48ee466d721da714dd009d856797
Ella Robinson <ellaroosmarijn@gmail.com>
2024-05-06 18:44:33 +0200

    feat: add input component with styling

---
+++ src/components/input/input.tsx
@@ -0,0 +1,89 @@
+import { ComponentProps, useEffect, useState } from "react"
+import styles from "./input.module.css"
+import cx from "classnames"
+import { X } from "lucide-react"
+
+export type InputProps = {
+  /** Placeholder for the input */
+  placeholder: string
+  /** Whether the input is invalid */
+  invalid?: boolean
+  /** Optional error on the input */
+  errorMessage?: string | null
+  /** Whether the input is disabled */
+  disabled?: boolean
+  /** Optional extra info text for the field */
+  info?: string
+  /** Whether the input is clearable */
+  clearable?: boolean
+  /** Icon */
+  icon?: any
+  /** Callback on clear */
+  onClear?(): void
+} & ComponentProps<"input">
+
+export function Input({
+  placeholder,
+  invalid,
+  errorMessage,
+  disabled,
+  info,
+  clearable,
+  onClear,
+  value,
+  onChange,
+  className,
+  icon: Icon,
+  ...props
+}: InputProps) {
+  const [inputValue, setInputValue] = useState<any>(value)
+  const [active, setActive] = useState(false)
+  const [isInvalid, setIsInvalid] = useState(false)
+
+  useEffect(() => {
+    setActive(!!inputValue)
+  }, [inputValue])
+
+  useEffect(() => {
+    setIsInvalid(invalid ?? false)
+    if (value && String(value).length > 0) {
+      setIsInvalid(false)
+    }
+  }, [invalid, value])
+
+  return (
+    <div className={cx(styles["input-container"], className)}>
+      <input
+        className={cx(
+          styles["input-field"],
+          disabled && styles.disabled,
+          active && styles.active,
+          (isInvalid ?? errorMessage) && styles.error
+        )}
+        value={inputValue}
+        placeholder={placeholder}
+        disabled={disabled}
+        onChange={(e) => {
+          setInputValue(e.target.value)
+          onChange?.(e)
+        }}
+        {...props}
+      />
+      {clearable && !isInvalid && !!inputValue && (
+        <X
+          className={styles.icon}
+          data-testid="input-clear"
+          onClick={() => setInputValue("")}
+        />
+      )}
+      {info && (
+        <div className={cx(styles.info, disabled && styles["disabled-info"])}>
+          {info}
+        </div>
+      )}
+      {errorMessage && isInvalid && (
+        <div className={styles["error-message"]}>{errorMessage}</div>
+      )}
+    </div>
+  )
+}
