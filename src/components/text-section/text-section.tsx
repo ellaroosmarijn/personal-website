83c36eb0cd4a7073bd8c850c58b49ff09e0d5936
Ella Robinson <ellaroosmarijn@gmail.com>
2024-05-08 18:56:34 +0200

    feat: add text section component

---
+++ src/components/text-section/text-section.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps } from "react"
+import styles from "./text-section.module.css"
+import cx from "classnames"
+
+type TextSectionProps = {
+  textColor: "white" | "black"
+} & ComponentProps<"div">
+
+export function TextSection({
+  textColor,
+  children,
+  className,
+  ...props
+}: TextSectionProps) {
+  return (
+    <div className={cx(styles["text-section-container"], className)} {...props}>
+      <h4 className={cx(styles.text, styles[textColor])}>{children}</h4>
+    </div>
+  )
+}
