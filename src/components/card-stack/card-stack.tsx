fe14415def7f97c61de108ed208b9c18f7fdacfc
Ella Robinson <ellaroosmarijn@gmail.com>
2024-06-08 21:28:04 +0200

    feat: add title, information, button, and media to card-stack

--- src/components/card-stack/card-stack.tsx
+++ src/components/card-stack/card-stack.tsx
@@ -1,35 +1,72 @@
 import { ComponentProps } from "react"
 import styles from "./card-stack.module.css"
 import { Button } from "../button/button"
-
+import cx from "classnames"
 type CardStackProps = {
-  color: "olive" | "yellow" | "brown"
+  color: number
   title: string
   description: string
+  extraInformation?: string
   image?: { src: string; alt: string }
-  buttonText: string
-  buttonLink: string
+  video?: {
+    src: string
+    type: string
+  }
+  primaryButton: {
+    theme: "ghost" | "secondary" | "primary"
+    text: string
+    link: string
+  }
+  secondaryButton?: { text: string; link: string }
 } & ComponentProps<"div">
 
 export function CardStack({
   color,
   title,
   description,
+  extraInformation,
   image,
-  buttonText,
-  buttonLink,
+  video,
+  primaryButton,
+  secondaryButton,
+  className,
   ...props
 }: CardStackProps) {
   return (
-    <div className={styles["card-stack"]} {...props} data-testid={"card-stack"}>
-      <div className={styles[`${color}`]}>
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <Button theme="ghost" label={buttonText} />
-        <Button theme="secondary" label={buttonText} />
-        <Button theme="primary" label={buttonText} />
-        {image && <img src={image.src} alt={image.alt} />}
+    <div
+      className={cx(className, styles["card-stack"], styles[`olive-${color}`])}
+      {...props}
+      data-testid={"card-stack"}
+    >
+      <div className={styles.information}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.description}>{description}</p>
+        {extraInformation && (
+          <p className={styles["extra-information"]}>{extraInformation}</p>
+        )}
+        <div className={styles["button-container"]}>
+          <Button
+            theme={primaryButton.theme}
+            label={primaryButton.text}
+            href={primaryButton.link}
+            className={styles["primary-button"]}
+          />
+          {secondaryButton && (
+            <Button
+              theme="ghost"
+              label={secondaryButton.text}
+              href={secondaryButton.link}
+            />
+          )}
+        </div>
       </div>
+      {image ? (
+        <img className={styles.image} src={image.src} alt={image.alt} />
+      ) : video ? (
+        <video className={styles.video} controls>
+          <source src={video.src} type={video.type} />
+        </video>
+      ) : null}
     </div>
   )
 }
