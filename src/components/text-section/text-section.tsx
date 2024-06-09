8ad719ea4bd641ae1437c79878e15f883e1a20e5
Ella Robinson <ellaroosmarijn@gmail.com>
2024-06-09 22:39:52 +0200

    feat: make title optional, add children and textcolor options

--- src/components/text-section/text-section.tsx
+++ src/components/text-section/text-section.tsx
@@ -4,17 +4,26 @@ import cx from "classnames"
 
 type TextSectionProps = {
   textColor?: "white" | "black"
+  title?: string
 } & ComponentProps<"div">
 
 export function TextSection({
-  textColor = "black",
+  textColor,
+  title,
   children,
   className,
   ...props
 }: TextSectionProps) {
   return (
     <div className={cx(styles["text-section-container"], className)} {...props}>
-      <h4 className={cx(styles.text, styles[textColor])}>{children}</h4>
+      {title && (
+        <h1 className={cx(styles.title, textColor && styles[textColor])}>
+          Welcome to my projects showcase!
+        </h1>
+      )}
+      <h4 className={cx(styles.text, textColor && styles[textColor])}>
+        {children}
+      </h4>
     </div>
   )
 }
